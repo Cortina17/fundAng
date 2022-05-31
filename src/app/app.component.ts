3b173c922b405c5a3ed0d819e044d0fc5ea5ab7b
diego <diego.cortinagarcia@gmail.com>
2022-05-31 09:33:37 +0200

    changed title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'myapp';
+  title = 'Angular';
+
+
+  changeTitle(){
+    this.title="Nuevo título";
+  }
 }
